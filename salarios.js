58c11f0a791b520027b52db68b77673dbd322dcd
karyrodriguez <kary.github@gmail.com>
2022-03-23 11:49:22 -0600

    Agregar archivos de salario y obtener la media general de salarios de un arreglo de objetos

---
+++ salarios.js
@@ -0,0 +1,41 @@
+const colombia = [];
+
+colombia.push({
+  name: "Karys",
+  salary: 10000,
+});
+
+colombia.push({
+  name: "Yiya",
+  salary: 20000,
+});
+
+colombia.push({
+  name: "Juan",
+  salary: 10400,
+});
+
+colombia.push({
+  name: "Pedro",
+  salary: 20320,
+});
+
+colombia.push({
+  name: "Laura",
+  salary: 14000,
+});
+
+colombia.push({
+  name: "Sergio",
+  salary: 21900,
+});
+
+colombia.push({
+  name: "Maria",
+  salary: 250345,
+});
+
+colombia.push({
+  name: "Pablo",
+  salary: 20560,
+});
